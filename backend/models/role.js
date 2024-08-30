0b7b99bbea6c88b927d13385291bf37f58b176ce
“Manuel” <carrizomanu12@gmail.com>
2024-08-30 13:05:07 -0300

    Arreglo Entidad Role

--- backend/models/role.js
+++ backend/models/role.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // Definir la asociación aquí
-      Role.hasMany(models.Product, {
+      Role.hasMany(models.User, {
         foreignKey: 'roleId',
         as: 'users'
       });
