94817f3557a25412d235ac964d2796e7b6ac4e2b
“Manuel” <carrizomanu12@gmail.com>
2024-08-30 12:44:29 -0300

    Entidad Servicie

---
+++ backend/models/service.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Service extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Definir la asociación aquí
+
+    }
+  }
+
+  Service.init({
+    name:{
+         type: DataTypes.STRING,
+         unique: true,
+        allowNull: false
+    },
+    description: DataTypes.STRING,
+  }, {
+    sequelize,  
+    modelName: 'Service',
+  });
+
+  return Service;
+};
